1460eecb14a06cd10d3deb4403900a4ee3183df5
Ông Tùng Dương <ongtungduong@gmail.com>
2025-04-05 23:55:40 +0700

    fix: fix indentation

--- wireguard-install.sh
+++ wireguard-install.sh
@@ -424,7 +424,7 @@ function listClients() {
 }
 
 function getClient() {
-    NUMBER_OF_CLIENTS=$(grep -c -E "^### Client" "/etc/wireguard/${SERVER_WG_NIC}.conf")
+	NUMBER_OF_CLIENTS=$(grep -c -E "^### Client" "/etc/wireguard/${SERVER_WG_NIC}.conf")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
 		echo ""
 		echo "You have no existing clients!"
